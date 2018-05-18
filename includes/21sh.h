655533c155fb414db9231d7b8f824484b54fae9d
vliubko <vliubko@student.unit.ua>
2018-05-18 12:35:16 +0300

    [ADD] Makefile, header, Cmake

---
+++ includes/21sh.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   21sh.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vliubko <vliubko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/20 14:40:30 by vliubko           #+#    #+#             */
+/*   Updated: 2018/05/18 12:21:11 by vliubko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SELECT_H
+# define FT_SELECT_H
+
+# include "../libft/includes/libft.h"
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <term.h>
+# include <signal.h>
+# include <termcap.h>
+
+typedef struct		s_args
+{
+	char			*value;
+	int				select;
+	int				underline;
+	int				type;
+	int				row;
+	struct s_args	*next;
+	struct s_args	*prev;
+}					t_args;
+
+typedef struct		s_select
+{
+	int				length;
+	struct termios	tty;
+	struct termios	savetty;
+	struct winsize	win;
+	struct s_args	*args;
+	int				cols;
+}					t_select;
+
+# define BOLD_WHINE_FONT "\e[1;37m"
+# define BG_COLOR "\e[48;5;29m"
+# define RED_FONT "\033[1;31m"
+# define CYAN_FONT "\e[1;36m"
+# define PURPLE_FONT "\e[1;35m"
+# define YELLOW_FONT "\e[38;5;226m"
+# define COLOR_OFF "\e[0m"
+# define ESC 		32539
+# define ESC_ALTERN	27
+# define BACKSPACE	127
+# define BACKSPACE_1 32639
+# define DEL			2117294875
+# define ENTER		10
+# define ENTER_1		32522
+# define SPACE		32544
+# define SPACE_1		32
+# define UP_ARROW	4283163
+# define DOWN_ARROW	4348699
+# define LEFT_ARROW	4479771
+# define RIGHT_ARROW	4414235
+
+#endif
